cb4b91b80b463181036f4b3a641df616d7396f83
gustavo-em <gustavo.emanuel01@outlook.com>
2022-05-09 08:44:31 -0300

    front end

--- android/app/src/main/java/com/testproj/MainApplication.java
+++ android/app/src/main/java/com/testproj/MainApplication.java
@@ -14,8 +14,11 @@ import com.testproj.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.BV.LinearGradient.LinearGradientPackage;
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 
 public class MainApplication extends Application implements ReactApplication {
+ 
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -37,6 +40,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
         }
       };
 
